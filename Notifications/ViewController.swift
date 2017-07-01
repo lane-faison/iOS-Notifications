72a10369f24382678964882583dfdcc3fc798720
Lane Faison <lane.faison@icloud.com>
2017-07-01 11:30:34 -0600

    Notifications properly sent to phone

--- Notifications/ViewController.swift
+++ Notifications/ViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 1. REQUEST PERMISSION
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, . sound], completionHandler: { (granted, error) in
+        
+            if granted {
+                print("Notification access granted")
+            } else {
+                print(error?.localizedDescription as Any)
+            }
+        })
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func notifyBtnTapped(sender: UIButton) {
+        scheduleNotification(inSeconds: 5, completion: { success in
+            if success {
+                print("Successfully scheduled notification")
+            } else {
+                print("Error scheduling notification")
+            }
+        })
     }
-
+    
+    func scheduleNotification(inSeconds: TimeInterval, completion: @escaping (_ Success: Bool) -> ()) {
+        
+        let notif = UNMutableNotificationContent()
+        notif.title = "New Notification"
+        notif.subtitle = "These are great!"
+        notif.body = "This is the body of your very first iOS Notification using Xcode and Swift 3"
+        
+        let notifTrigger = UNTimeIntervalNotificationTrigger(timeInterval: inSeconds, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "myNotification", content: notif, trigger: notifTrigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+            if error != nil {
+                print(error!)
+                completion(false)
+            } else {
+                completion(true)
+            }
+        
+        })
+    }
 
 }
 
